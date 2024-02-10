487568facb8f7961d9dc80b0b1722b7c75c12f1c
Khai Huyen Nguyen <50721063+Khaihuyennguyen@users.noreply.github.com>
2024-02-10 15:10:56 -0600

    Add project files.

---
+++ Models/BTUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+
+namespace TheBugTracker.Models
+{
+    public class BTUser : IdentityUser
+    {
+
+    }
+}
