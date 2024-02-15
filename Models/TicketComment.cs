9aa521b64a8e20c2fb8c6c001dbc4a0d31758b5e
Khai Huyen Nguyen <50721063+Khaihuyennguyen@users.noreply.github.com>
2024-02-14 23:19:16 -0600

    ticket comment

---
+++ Models/TicketComment.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+namespace TheBugTracker.Models
+{
+    public class TicketComment
+    {
+        //ID
+        public int Id { get; set; }
+        //Comment
+        
+        [DisplayName("Comment")]
+        public string Comment { get; set; }
+        //Craeted
+
+        [DisplayName("Date Modified")]
+        public DateTimeOffset Created { get; set; }
+        //TicketID
+        [DisplayName("Ticket")]
+        public int TicketId { get; set; }
+        //userID
+        
+        [DisplayName("Team Member")]
+        public string UserId { get; set; }
+        //Navigation Properties
+        public virtual Ticket Ticket { get; set; }
+        public virtual BTUser User { get; set; }
+    }
+}
