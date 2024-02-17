3c46004d8a70777f4da144d9ab8552e7cb34ce6f
Khai Huyen Nguyen <50721063+Khaihuyennguyen@users.noreply.github.com>
2024-02-16 23:46:41 -0600

    Project Priority

---
+++ Models/ProjectPriority.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+namespace TheBugTracker.Models
+{
+    public class ProjectPriority
+    {
+        public int Id { get; set; }
+        [DisplayName("Priority Name")]
+        public string Name { get; set; }    
+    }
+}
