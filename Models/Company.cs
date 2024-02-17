7788a68b2ccae8ed8ba963d735b1b601d755e032
Khai Huyen Nguyen <50721063+Khaihuyennguyen@users.noreply.github.com>
2024-02-17 00:08:43 -0600

    Company

--- Models/Company.cs
+++ Models/Company.cs
@@ -1,6 +1,27 @@
-﻿namespace TheBugTracker.Models
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.CodeAnalysis;
+
+namespace TheBugTracker.Models
 {
     public class Company
     {
+        //Primary key
+        public int Id { get; set; }
+        [DisplayName("Company Name")]
+        public string Name { get; set; }
+        [DisplayName("Company Description")]
+        public string Description { get; set; }
+        //--Navigtation properties --//
+
+        public virtual ICollection<BTUser> Members { get; set; }
+        public virtual ICollection<Project> Projects { get; set; }
+
     }
 }
