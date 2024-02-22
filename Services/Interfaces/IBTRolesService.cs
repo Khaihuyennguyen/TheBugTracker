0e34c61bf0d69b3f46204a30be4cd20f2ff26e1d
Khai Huyen Nguyen <50721063+Khaihuyennguyen@users.noreply.github.com>
2024-02-21 22:37:00 -0600

    IBRoles services

---
+++ Services/Interfaces/IBTRolesService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using TheBugTracker.Models;
+
+namespace TheBugTracker.Services.Interfaces
+{
+    public interface IBTRolesService
+    {
+        public Task<bool> IsUserInRoleAsync(BTUser user, string roleName);
+
+        public Task<IEnumerable<string>> GetUserRolesAsync(BTUser user);
+
+        public Task<bool> AddUserToRoleAsync(BTUser user, string roleName);
+
+        public Task<bool> RemoveUserFromRoleAsync(BTUser user, string roleName);
+
+        public Task<bool> RemoveuserFromRolesAsync(BTUser user, IEnumerable<string> roles);
+
+        public Task<List<BTUser>> GetUsersInRoleAsync(string roleName, int companyId);
+
+        public Task<List<BTUser>> GetUsersNotInRoleAsync(string roleName, int companyId);
+
+        public Task<string> GetRoleNameByIdAsync(string roleId);
+    }
+}
