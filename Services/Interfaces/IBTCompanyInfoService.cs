8107beac3e5b78f77bd0089f40a55b96d471d39c
Khai Huyen Nguyen <50721063+Khaihuyennguyen@users.noreply.github.com>
2024-02-26 23:31:48 -0600

    IBTCompanyInfoService

---
+++ Services/Interfaces/IBTCompanyInfoService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using TheBugTracker.Models;
+
+namespace TheBugTracker.Services.Interfaces
+{
+    public interface IBTCompanyInfoService
+    {
+        public Task<Company> GetCompanyInfoByIdAsync(int? companyId);
+        public Task<List<BTUser>> GetAllMembersAsync(int companyId);
+
+        public Task<List<Project>> GetAllProjectsAsync(int companyId);
+        public Task<List<Ticket>> GetAllTicketAsync(int compaynyId);
+
+
+    }
+}
