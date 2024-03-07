1dbdedde25218363c26762df35071267aaa90b28
Khai Huyen Nguyen <50721063+Khaihuyennguyen@users.noreply.github.com>
2024-03-07 17:22:38 -0600

    service email and notification

---
+++ Services/BTEmailService.cs
@@ -0,0 +1,53 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Options;
+using TheBugTracker.Models;
+using System;
+using System.Collections.Generic;
+using MimeKit;
+
+using MailKit.Net.Smtp;
+using MailKit.Security;
+
+namespace TheBugTracker.Services
+{
+    public class BTEmailService : IEmailSender
+    {
+        private readonly MailSettings _mailSettings;
+        // Ctor and tabs will do the job
+        public BTEmailService(IOptions<MailSettings> mailSettings)
+        {
+            _mailSettings = mailSettings.Value;
+        }
+        public async Task SendEmailAsync(string emailTo, string subject, string htmlMessage)
+        {
+            MimeMessage email = new();
+            email.Sender = MailboxAddress.Parse(_mailSettings.Mail);
+            email.To.Add(MailboxAddress.Parse(emailTo));
+            email.Subject = subject;
+
+            var builder = new BodyBuilder
+            {
+                HtmlBody = htmlMessage
+            };
+
+            email.Body = builder.ToMessageBody();
+
+            try
+            {
+                using var smtp = new SmtpClient();
+                smtp.Connect(_mailSettings.Host, _mailSettings.Port, SecureSocketOptions.StartTls);
+                smtp.Authenticate(_mailSettings.Mail, _mailSettings.Password);
+
+                await smtp.SendAsync(email);
+
+                smtp.Disconnect(true);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
