bbb1ae69859a36d5be692e7e3f47adfc4bac34d2
Khai Huyen Nguyen <50721063+Khaihuyennguyen@users.noreply.github.com>
2024-03-08 00:04:13 -0600

    claims implementation user

---
+++ Services/Factories/BTUserClaimsPrincipleFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using TheBugTracker.Models;
+
+namespace TheBugTracker.Services.Factories
+{
+    public class BTUserClaimsPrincipleFactory : UserClaimsPrincipalFactory<BTUser, IdentityRole>
+    {
+        public BTUserClaimsPrincipleFactory(UserManager<BTUser> userManager,
+                                            RoleManager<IdentityRole> roleManager,
+                                            IOptions<IdentityOptions> optionsAccessor)
+            : base(userManager, roleManager, optionsAccessor)
+        {
+
+        }
+
+        protected override async Task<ClaimsIdentity> GenerateClaimsAsync(BTUser user)
+        {
+            ClaimsIdentity identity = await base.GenerateClaimsAsync(user);
+            identity.AddClaim(new Claim("CompanyId", user.CompanyId.ToString()));
+
+
+            return identity;
+        }
+    }
+}
