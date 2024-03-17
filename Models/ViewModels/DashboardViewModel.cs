520c7e9ef2f26e59f7cfedd49785c7036b5632ac
Khai Huyen Nguyen <50721063+Khaihuyennguyen@users.noreply.github.com>
2024-03-16 21:22:26 -0500

    homecontroller of dashboard

---
+++ Models/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using TheBugTracker.Controllers;
+
+namespace TheBugTracker.Models.ViewModels
+{
+    public class DashboardViewModel
+    {
+        public Company Company { get; set; }
+        public List<Project> Projects { get; set; }
+        public List<Ticket> Tickets { get; set; }
+        public List<BTUser> Members { get; set; }
+    }
+}
